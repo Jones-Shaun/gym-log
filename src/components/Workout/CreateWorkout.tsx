512567deb29036e85489d4a798eee2f3ffc903d1
Shaun Jones <jonesshaun2021@gmail.com>
2025-02-17 15:43:38 -0500

    Added an add button to the workout creation form for exercises

--- src/components/Workout/CreateWorkout.tsx
+++ src/components/Workout/CreateWorkout.tsx
@@ -1,14 +1,17 @@
 export default function CreateWorkout() {
 	return (
 		<div className="h-full w-full flex flex-col text-primary/60 items-center justify-center text-4xl gap-12">
-			<h1>New Workout!</h1>
-			<form className="flex flex-col gap-4 w-1/2">
+			<form className="flex flex-col gap-4 w-3/4 p-10 overflow-y-scroll">
+				<h1>New Workout!</h1>
 				<label className="text-lg">
 					Workout Name:
 					<input type="text" className="border p-2 w-full" placeholder="Enter workout name" />
 				</label>
 				<div className="flex flex-col gap-4">
-					<h2 className="text-2xl">Exercises</h2>
+					<div className="flex justify-between items-center">
+						<h2 className="text-2xl">Exercises</h2>
+						<button type="button" className="bg-primary text-white p-2 text-xl">Add</button>
+					</div>
 					<div className="flex flex-col gap-4">
 						<div className="flex flex-col gap-2 border p-4">
 							<label className="text-lg">
@@ -29,7 +32,6 @@ export default function CreateWorkout() {
 							</label>
 						</div>
 					</div>
-					<button type="button" className="bg-primary text-white p-2 mt-4">Add Another Exercise</button>
 				</div>
 				<button type="submit" className="bg-primary text-white p-2 mt-4">Create</button>
 			</form>
