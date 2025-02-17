b533a11ad74e8e363dd3dc3f066f0fc6eaf8d8e0
Shaun Jones <jonesshaun2021@gmail.com>
2025-02-17 18:44:18 -0500

    fix

--- src/components/History/WorkoutHistoryInfo.tsx
+++ src/components/History/WorkoutHistoryInfo.tsx
@@ -1,9 +1,6 @@
 import { useParams } from "react-router";
 import { WorkoutInterface } from "../../util/interfaces";
-<<<<<<< HEAD
-import db from "local-db-storage";
-=======
->>>>>>> f628fba653ac68b2e2eec6daf02fd73b324ea08b
+// import db from "local-db-storage";
 import WorkoutComplete from "../Dashboard/WorkoutComplete";
 import { useEffect, useState } from "react";
 import { fetchData } from "../../util/util";
