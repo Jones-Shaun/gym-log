64a83b4720e190ced956253e77ed716518cff6bf
tle57 <tle57@gmu.edu>
2025-02-17 19:32:15 -0500

    hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import App from "./pages/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<BrowserRouter basename="/gym-log">
+	<HashRouter>
 		<React.StrictMode>
 			<App />
 		</React.StrictMode>
-	</BrowserRouter>
+	</HashRouter>
 );
